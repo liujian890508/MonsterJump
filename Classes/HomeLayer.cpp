434556df4392b40a1e3bccef748f4b9a7cec1a7e
liujian890508 <liujian890508@gmail.com>
2014-11-18 13:27:59 +0800

    update

--- Classes/HomeLayer.cpp
+++ Classes/HomeLayer.cpp
@@ -4,6 +4,7 @@
 #include "GameWorld.h"
 #include "ShareManager.h"
 #include "AdManager.h"
+#include "Background.h"
 
 HomeLayer::HomeLayer()
 {
@@ -18,18 +19,21 @@ bool HomeLayer::init()
 {
 	this->loadResource();
 
-	auto enterMenu = MenuItemFont::create("Play", [=](Ref *pSender){
+	auto background = Background::create();
+	this->addChild(background);
+
+	auto playerMenu = MenuItemImage::create("ui/btn_play.jpg", "ui/btn_play.jpg", [=](Ref *pSender){
         std::tuple<int, std::string> param = std::make_tuple(12, "Hello World");
 		Utils::replaceScene(GameWorld::create(), &param);
 	});
-    auto shareMenu = MenuItemFont::create("Share", [=](Ref* pSender){
+	auto shareMenu = MenuItemImage::create("ui/btn_share.png", "ui/btn_share.png", [=](Ref* pSender){
         ShareMgr->setDefaultContent();
         ShareMgr->sendShare();
     });
 	auto spotAds = MenuItemFont::create("SpotAds", [=](Ref* pSender){
 		AdMgr->showSpotAds();
 	});
-	auto menu = Menu::create(enterMenu, shareMenu, spotAds, nullptr);
+	auto menu = Menu::create(playerMenu, shareMenu, spotAds, nullptr);
     menu->alignItemsVerticallyWithPadding(30);
 	this->addChild(menu);
 	menu->setPosition(VisibleRect::center());
